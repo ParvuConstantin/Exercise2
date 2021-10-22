d8265abf07fffeb1661f49191f848c5a3978d7b2
Parvu Constantin <parvu_ctin99@yahoo.com>
2021-10-22 20:36:46 +0300

    Exercise3

--- src/ro/fasttrackit/curs6/homework/Exercise3.java
+++ src/ro/fasttrackit/curs6/homework/Exercise3.java
@@ -2,14 +2,14 @@ package ro.fasttrackit.curs6.homework;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        int[] numbers={31, 4, 89, 21, 17, 2};
+        int[] numbers = {31, 4, 89, 21, 17, 2};
         numereIntregi(numbers);
     }
 
-    public static void numereIntregi(int[] nr){
-        int numar=10;
-        for(int element:nr){
-            if(element>numar){
+    public static void numereIntregi(int[] nr) {
+        int numar = 10;
+        for (int element : nr) {
+            if (element > numar) {
                 System.out.println(element);
             }
         }
