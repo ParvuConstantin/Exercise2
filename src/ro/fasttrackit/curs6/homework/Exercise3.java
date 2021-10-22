4eb0e774f0aa692061e228afe65f9df005167019
Parvu Constantin <parvu_ctin99@yahoo.com>
2021-10-22 20:36:09 +0300

    Exercise3

---
+++ src/ro/fasttrackit/curs6/homework/Exercise3.java
@@ -0,0 +1,17 @@
+package ro.fasttrackit.curs6.homework;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int[] numbers={31, 4, 89, 21, 17, 2};
+        numereIntregi(numbers);
+    }
+
+    public static void numereIntregi(int[] nr){
+        int numar=10;
+        for(int element:nr){
+            if(element>numar){
+                System.out.println(element);
+            }
+        }
+    }
+}
